a61ba5e0c9e3714f5e044a337594f41534e8e112
ISAAC\laurie.copley <laurie.copley@isaac.nl>
2018-12-17 14:07:35 +0100

    Improved variable names in settings-toolbar.js

--- js/settings-toolbar.js
+++ js/settings-toolbar.js
@@ -1,35 +1,34 @@
 (function () {
     var methods = {
-      elements: {},
+      $elements: {},
       init: function (settingsToolbarContainerSelector) {
-          var settingsToolbarContainer = document.querySelector(settingsToolbarContainerSelector);
-          console.log(settingsToolbarContainerSelector, settingsToolbarContainer);
-        this.elements.settingsToolbar = settingsToolbarContainer.querySelector('.settings-toolbar');
-        this.elements.actionsButton = settingsToolbarContainer.querySelector('.setting.actions');
-        this.elements.behindColour = settingsToolbarContainer.querySelector('.behind-color');
-        this.elements.buttons = settingsToolbarContainer.querySelectorAll('[data-setting]');
+        var $settingsToolbarContainer = document.querySelector(settingsToolbarContainerSelector);
+        this.$elements.settingsToolbar = $settingsToolbarContainer.querySelector('.settings-toolbar');
+        this.$elements.actionsButton = $settingsToolbarContainer.querySelector('.setting.actions');
+        this.$elements.behindColour = $settingsToolbarContainer.querySelector('.behind-color');
+        this.$elements.buttons = $settingsToolbarContainer.querySelectorAll('[data-setting]');
   
-        this.elements.actionsButton.addEventListener('click', (function (e) {
+        this.$elements.actionsButton.addEventListener('click', (function (e) {
           e.preventDefault();
           this.toggleOpen();
         }).bind(this));
   
-        this.toggleExpandable(this.elements.buttons.length > 1);
+        this.toggleExpandable(this.$elements.buttons.length > 1);
       },
       toggleOpen: function () {
-        this.elements.settingsToolbar.classList.toggle('open');
-        this.elements.behindColour.classList.toggle('open');
-        this.elements.actionsButton.classList.toggle('open');
+        this.$elements.settingsToolbar.classList.toggle('open');
+        this.$elements.behindColour.classList.toggle('open');
+        this.$elements.actionsButton.classList.toggle('open');
   
-        for (var i = 0; i < this.elements.buttons.length; i++) {
-          this.elements.buttons[i].classList.toggle('open');
+        for (var i = 0; i < this.$elements.buttons.length; i++) {
+          this.$elements.buttons[i].classList.toggle('open');
         }
       },
       toggleExpandable: function (expandable) {
         if (!expandable) {
           this.toggleOpen(!expandable);
-          this.elements.behindColour.style.display = 'none';
-          this.elements.actionsButton.style.display = 'none';
+          this.$elements.behindColour.style.display = 'none';
+          this.$elements.actionsButton.style.display = 'none';
         }
       }
     };
